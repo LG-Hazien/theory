30b91559612f7ce17e44efbf6cda10b652f03123
LG-Hazien <144539093+LG-Hazien@users.noreply.github.com>
2023-09-10 15:48:30 -0300

    Create clubster-theory.js

---
+++ clubster-theory.js
@@ -0,0 +1,13 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "OuO";
+var name = "Clubster";
+var description = "No theory description :("
+var authors = "JojoGames";
+var version = "v1.0.0";
+
+var clubster, najor;
